b5a19b5025cc6b3884da7d6bfe0661fc90a37ade
Will Lauer <willlauer1@gmail.com>
2025-07-22 16:55:48 -0700

    WIP

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -12,3 +12,18 @@ class BasicTests(unittest.TestCase):
         """Test initialize and deinitialize"""
         sdk.initialize()
         sdk.deinitialize()
+
+    def test_replay(self):
+        """Test that replay works"""
+        sdk.initialize()
+        sdk.enable_frame_fifo(0, 10)
+        sdk.load_pcap("/root/Documents/cepton/lobby-1.pcap")
+
+        frame_counts = []
+        for _ in range(10):
+            frame = sdk.frame_fifo_get_frame(2000)
+            frame_counts.append(frame.positions.shape[0])
+
+        self.assertTrue(all(count > 0 for count in frame_counts))
+
+        sdk.deinitialize()
